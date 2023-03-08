5f6625d7798ca1ce5be1e3a91c5c53cc247f8ab8
webdev1903 <vantureprakash1903@gmail.com>
2023-03-09 01:57:28 +0530

    changed font style

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="bg-purple w-screen h-screen">
       <div className="flex w-max justify-center items-center gap-x-4 absolute top-28 left-10">
         <img src="./icons/activity.png" className="w-4 h-4" />
-        <p className="font-tomorrow non-italic font-semibold font-lbase uppercase text-trend opacity-90 tracking-tightest">
+        <p className="non-italic font-semibold font-lbase text-trend opacity-90 tracking-tightest">
           Trending Assets
         </p>
       </div>
